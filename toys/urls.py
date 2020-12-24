de67e8019ad5141ee2fc4ec6bbffd873c1c8d1c6
Ismoilwebdev <Ismoilwebdev@yandex.ru>
2020-12-24 08:50:21 +0500

    Add pythonanywhere to allowed hosts

--- toys/urls.py
+++ toys/urls.py
@@ -1,9 +1,5 @@
 from django.urls import path
-<<<<<<< HEAD
 from . import views
-=======
-from toys import views
->>>>>>> origin/master
 
 urlpatterns = [
     path('', views.dashboard, name="dashboard"),
